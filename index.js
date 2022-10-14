79c83b5a332e182ed2b243e61833f9e6c2ad13fc
Mayza <mayza.santos@outlook.com.br>
2022-10-14 08:33:39 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+//Exercício 6
+let contador = 0
+contador=contador+1
+console.log(contador)
+contador=contador+1
+console.log(contador)
+contador=contador+1
+console.log(contador)
+contador=contador+1
+console.log(contador)
+contador=contador+1
+console.log(contador)
+
+/*//Exercício 7
+let anoAtual= 2022
+let anoNAscimento = 1993
+let idadeAtual = anoAtual-anoNAscimento
+console.log(`No ano ${anoAtual}, eu tenho/vou ter ${idadeAtual} anos de idade`)*/
+
+//Exercício 8
+let alturaRetangulo = 5
+let larguraRetangulo = 7
+let areaRetangulo = ((alturaRetangulo*larguraRetangulo)/2)
+console.log (`Um retângulo com largura de ${larguraRetangulo}cm e altura de ${alturaRetangulo}cm, possui ${areaRetangulo}cm² de área total.`)
+
+//Exercício 9
+let diaAtual = 11
+let mesAtual = 08
+let anoAtual = 2022
+let dataAtualFormada = (diaAtual+"/"+mesAtual+"/"+anoAtual)
+console.log(`A data de hoje é ${dataAtualFormada}`)
+
+//Exercício 10
+let valorA = 64
+let valorB = 56
+let soma = valorA+valorB
+let valorA1=valorA=valorB
+let valorB1 =soma-valorA
+
+console.log (`Antes a variável ‘valorA’ possuía o valor ${valorA}, agora possui o valor ${valorA1}.`)
+console.log (`Antes a variável ‘valorB’ possuía o valor ${valorB}, agora possui o valor ${valorB1}.`)
+
+
+
+
+
+
+
